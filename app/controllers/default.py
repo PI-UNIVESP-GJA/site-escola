ac9ead0031157cb66bf53cb4d6e0f586ca8a368b
Frederico <fred.cominato@live.com>
2023-05-17 20:13:07 -0300

    visualizar csv

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,7 +1,7 @@
 from flask import render_template, redirect, url_for
 from app import app, db, login_manager
 from sqlalchemy import asc, and_
-import json
+import csv
 
 
 
@@ -9,12 +9,13 @@ import json
 def load_user(id):
     return User.query.filter_by(id=id).first()
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+#@app.route('/')
+#def index():
+#    return render_template('index.html')
 
-@app.route('/logout/')
-def logout():
-    logout_user()
-    error = "Sessão encerrada com sucesso"
-    return render_template('index.html', error=error)
+@app.route("/")
+def index():
+  with open("BCG.csv") as file:
+   reader = csv.reader(file, delimiter = ';')
+   header = next(reader)
+   return render_template("index.html", header=header, rows=reader)

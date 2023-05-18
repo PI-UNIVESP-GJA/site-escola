a439a0ca3d83557758965e74ba0cde3c272f43b6
Frederico <fred.cominato@live.com>
2023-05-18 11:06:37 -0300

    tentativa de atualizacao requirements

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,17 +1,15 @@
-from flask import render_template, redirect, url_for
+from flask import Flask, jsonify,render_template, redirect, url_for
 from app import app, db, login_manager
 from sqlalchemy import asc, and_
 import csv
-
+#import pandas as pd
+#import numpy as np
 
 
 @login_manager.user_loader
 def load_user(id):
     return User.query.filter_by(id=id).first()
 
-#@app.route('/')
-#def index():
-#    return render_template('index.html')
 
 @app.route("/")
 def index():
